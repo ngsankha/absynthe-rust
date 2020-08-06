c6fbb1224cb91d6e447e180ec4bb5ce6bfe7fce7
Sankha Narayan Guria <sankha93@gmail.com>
2020-08-06 13:36:22 -0400

    types all set

--- src/lib.rs
+++ src/lib.rs
@@ -1,108 +1,269 @@
 use std::collections::HashMap;
+use std::cmp::Ordering;
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Value {
+pub enum StrLenLat {
+    Top,
+    Len(usize),
+    Bot
+}
+
+trait Lattice: PartialOrd + PartialEq {
+    // fn meet(&self, other: &Self) -> Self;
+    fn join(&self, other: &Self) -> Self;
+}
+
+impl PartialOrd for StrLenLat {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self {
+            StrLenLat::Top => match other {
+                StrLenLat::Top => Some(Ordering::Equal),
+                _ => Some(Ordering::Greater)
+            },
+            StrLenLat::Len(x) => match other {
+                StrLenLat::Top => Some(Ordering::Less),
+                StrLenLat::Len(y) => x.partial_cmp(y),
+                StrLenLat::Bot => Some(Ordering::Greater)
+            },
+            StrLenLat::Bot => match other {
+                StrLenLat::Bot => Some(Ordering::Equal),
+                _ => Some(Ordering::Less)
+            }
+        }
+    }
+}
+
+impl Lattice for StrLenLat {
+    fn join(&self, other: &Self) -> Self {
+        match self {
+            StrLenLat::Top => self.clone(),
+            StrLenLat::Len(v1) => match other {
+                StrLenLat::Top => other.clone(),
+                StrLenLat::Len(v2) => if v1 > v2 {
+                    StrLenLat::Len(*v1)
+                } else {
+                    StrLenLat::Len(*v2)
+                },
+                StrLenLat::Bot => self.clone()
+            },
+            StrLenLat::Bot => other.clone()
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum AbsValue<T, U> {
+    Abs(T),
+    Conc(U)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum StrVal {
     Str(String),
     Int(i64),
     Error
 }
 
-pub enum Func {
-    Append(Expr, Expr),
-    Replace(Expr, Expr, Expr),
-    Substr(Expr, Expr, Expr),
-    Add(Expr, Expr),
-    Sub(Expr, Expr),
-    Len(Expr)
+pub enum Func<T> {
+    Append(Expr<T>, Expr<T>),
+    // Replace(Expr<T>, Expr<T>, Expr<T>),
+    // Substr(Expr<T>, Expr<T>, Expr<T>),
+    // Add(Expr<T>, Expr<T>),
+    // Sub(Expr<T>, Expr<T>),
+    // Len(Expr<T>)
 }
 
-pub enum Expr {
-    Const(Value),
+pub enum Expr<T> {
+    Const(T),
     Var(String),
-    Call(Box<Func>)
+    Call(Box<Func<T>>)
 }
 
-pub fn interp(expr: &Expr, env: &HashMap<String, Value>) -> Value {
-    match expr {
-        Expr::Const(v) => v.clone(),
-        Expr::Var(x) => match env.get(x) {
-            Some(val) => val.clone(),
-            None => Value::Error
+pub trait Value: Clone {
+    fn error() -> Self;
+}
+
+trait Abstractable<T> {
+    fn abstraction(&self) -> T;
+}
+
+impl Abstractable<AbsStrVal> for StrVal {
+    fn abstraction(&self) -> AbsStrVal {
+        match self {
+            StrVal::Str(s) => AbsValue::Abs(StrLenLat::Len(s.chars().count())),
+            _ => AbsValue::Conc(self.clone())
         }
-        Expr::Call(call) => eval_call(call, env)
     }
 }
 
-fn eval_call(call: &Func, env: &HashMap<String, Value>) -> Value {
-    match call {
-        Func::Append(s1, s2) => str_append(interp(s1, env), interp(s2, env)),
-        Func::Replace(s1, s2, s3) => str_replace(interp(s1, env), interp(s2, env), interp(s3, env)),
-        Func::Substr(s1, s2, s3) => str_substr(interp(s1, env), interp(s2, env), interp(s3, env)),
-        Func::Add(i, j) => int_add(interp(i, env), interp(j, env)),
-        Func::Sub(i, j) => int_sub(interp(i, env), interp(j, env)),
-        Func::Len(s) => str_len(interp(s, env))
+impl Value for StrVal {
+    fn error() -> Self {
+        StrVal::Error
     }
 }
 
-fn str_append(v1: Value, v2: Value) -> Value {
-    match v1 {
-        Value::Str(s1) => match v2 {
-            Value::Str(s2) => Value::Str(s1 + &s2),
-            _ => Value::Error
-        },
-        _ => Value::Error
+type AbsStrVal = AbsValue<StrLenLat, StrVal>;
+
+impl Value for AbsStrVal {
+    fn error() -> Self {
+        AbsValue::Conc(StrVal::Error)
     }
 }
 
-fn str_replace(v1: Value, v2: Value, v3: Value) -> Value {
-    match v1 {
-        Value::Str(s1) => match v2 {
-            Value::Str(s2) => match v3 {
-                Value::Str(s3) => Value::Str(s1.replace(&s2, &s3)),
-                _ => Value::Error
+pub trait Interpreter<T: Value> {
+    fn eval(expr: &Expr<T>, env: &HashMap<String, T>) -> T {
+        match expr {
+            Expr::Const(v) => v.clone(),
+            Expr::Var(x) => match env.get(x) {
+                Some(val) => val.clone(),
+                None => T::error()
             }
-            _ => Value::Error
-        },
-        _ => Value::Error
+            Expr::Call(call) => Self::eval_call(call, env)
+        }
+    }
+
+    fn eval_call(expr: &Func<T>, env: &HashMap<String, T>) -> T;
+}
+
+pub struct StrOpInterpreter;
+
+pub struct StrLenInterp;
+
+impl Interpreter<AbsStrVal> for StrLenInterp {
+    fn eval_call(expr: &Func<AbsStrVal>, env: &HashMap<String, AbsStrVal>) -> AbsStrVal {
+        match expr {
+            Func::Append(s1, s2) => Self::abs_str_append(Self::eval(s1, env), Self::eval(s2, env)),
+            // Func::Replace(s1, s2, s3) => str_replace(interp(s1, env), interp(s2, env), interp(s3, env)),
+            // Func::Substr(s1, s2, s3) => str_substr(interp(s1, env), interp(s2, env), interp(s3, env)),
+            // Func::Add(i, j) => int_add(interp(i, env), interp(j, env)),
+            // Func::Sub(i, j) => int_sub(interp(i, env), interp(j, env)),
+            // Func::Len(s) => str_len(interp(s, env))
+        }
     }
 }
 
-fn str_substr(v1: Value, v2: Value, v3: Value) -> Value {
-    match v1 {
-        Value::Str(s1) => match v2 {
-            Value::Int(start) => match v3 {
-                Value::Int(end) => Value::Str(s1.chars().skip(start as usize).take((end - start) as usize).collect()),
-                _ => Value::Error
-            }
-            _ => Value::Error
-        },
-        _ => Value::Error
+impl StrLenInterp {
+    fn abs_str_append(v1: AbsStrVal, v2: AbsStrVal) -> AbsStrVal {
+        match (v1.clone(), v2.clone()) {
+            (AbsValue::Abs(s1), AbsValue::Abs(s2)) => match (s1, s2) {
+                (StrLenLat::Top, _) => AbsValue::Abs(StrLenLat::Top),
+                (_, StrLenLat::Top) => AbsValue::Abs(StrLenLat::Top),
+                (StrLenLat::Len(l1), StrLenLat::Len(l2)) => AbsValue::Abs(StrLenLat::Len(l1 + l2)),
+                (StrLenLat::Len(l1), StrLenLat::Bot) => AbsValue::Abs(StrLenLat::Len(l1)),
+                (StrLenLat::Bot, StrLenLat::Len(l2)) => AbsValue::Abs(StrLenLat::Len(l2)),
+                (_, _) => AbsValue::Abs(StrLenLat::Bot)
+            },
+            (AbsValue::Conc(c1), AbsValue::Conc(c2)) => match (c1.clone(), c2.clone()) {
+                (StrVal::Str(s1), StrVal::Str(s2)) => AbsValue::Conc(StrVal::Str(s1 + &s2)),
+                (_, _) => AbsValue::Conc(StrVal::Error)
+            },
+            (AbsValue::Conc(c), AbsValue::Abs(_)) => Self::abs_str_append(c.abstraction(), v2),
+            (AbsValue::Abs(_), AbsValue::Conc(c)) => Self::abs_str_append(v1, c.abstraction()),
+        }
     }
 }
 
-fn int_add(v1: Value, v2: Value) -> Value {
-    match v1 {
-        Value::Int(i) => match v2 {
-            Value::Int(j) => Value::Int(i + j),
-            _ => Value::Error
-        },
-        _ => Value::Error
+impl Interpreter<StrVal> for StrOpInterpreter {
+    fn eval_call(call: &Func<StrVal>, env: &HashMap<String, StrVal>) -> StrVal {
+        match call {
+            Func::Append(s1, s2) => Self::str_append(Self::eval(s1, env), Self::eval(s2, env)),
+            // Func::Replace(s1, s2, s3) => str_replace(interp(s1, env), interp(s2, env), interp(s3, env)),
+            // Func::Substr(s1, s2, s3) => str_substr(interp(s1, env), interp(s2, env), interp(s3, env)),
+            // Func::Add(i, j) => int_add(interp(i, env), interp(j, env)),
+            // Func::Sub(i, j) => int_sub(interp(i, env), interp(j, env)),
+            // Func::Len(s) => str_len(interp(s, env))
+        }
     }
 }
 
-fn int_sub(v1: Value, v2: Value) -> Value {
-    match v1 {
-        Value::Int(i) => match v2 {
-            Value::Int(j) => Value::Int(i - j),
-            _ => Value::Error
-        },
-        _ => Value::Error
+impl StrOpInterpreter {
+    fn str_append(v1: StrVal, v2: StrVal) -> StrVal {
+        match (v1, v2) {
+            (StrVal::Str(s1), StrVal::Str(s2)) => StrVal::Str(s1 + &s2),
+            _ => StrVal::error()
+        }
     }
 }
 
-fn str_len(v: Value) -> Value {
-    match v {
-        Value::Str(s) => Value::Int(s.chars().count() as i64),
-        _ => Value::Error
-    }
-}
+// pub fn interp<T: Value, Clone>(expr: &Expr<T>, env: &HashMap<String, T>) -> T {
+//     match expr {
+//         Expr::Const(v) => *v.clone(),
+//         Expr::Var(x) => match env.get(x) {
+//             Some(val) => *val.clone(),
+//             None => T::error()
+//         }
+//         Expr::Call(call) => eval_call(call, env)
+//     }
+// }
+
+// fn eval_call<T>(call: &Func<T>, env: &HashMap<String, T>) -> T {
+//     match call {
+//         Func::Append(s1, s2) => str_append(interp(s1, env), interp(s2, env)),
+//         // Func::Replace(s1, s2, s3) => str_replace(interp(s1, env), interp(s2, env), interp(s3, env)),
+//         // Func::Substr(s1, s2, s3) => str_substr(interp(s1, env), interp(s2, env), interp(s3, env)),
+//         // Func::Add(i, j) => int_add(interp(i, env), interp(j, env)),
+//         // Func::Sub(i, j) => int_sub(interp(i, env), interp(j, env)),
+//         // Func::Len(s) => str_len(interp(s, env))
+//     }
+// }
+
+// fn str_append<T>(v1: StrVal, v2: StrVal) -> StrVal {
+//     match (v1, v2) {
+//         (StrVal::Str(s1), StrVal::Str(s2)) => StrVal::Str(s1 + &s2),
+//         _ => StrVal::error()
+//     }
+// }
+
+// fn abs_str_append(v1: StringLenLattice, v2: StringLenLattice) -> StringLenLattice {
+//     match (v1, v2) {
+//         (StringLenLattice::Top, _) => StringLenLattice::Top,
+//         (_, StringLenLattice::Top) => StringLenLattice::Top,
+//         (StringLenLattice::Len(l1), StringLenLattice::Len(l2)) => StringLenLattice::Len(l1 + l2),
+//         (StringLenLattice::Len(_), StringLenLattice::Bot) => StringLenLattice::Bot,
+//         (StringLenLattice::Bot, StringLenLattice::Len(_)) => StringLenLattice::Bot,
+//         (StringLenLattice::Bot, StringLenLattice::Bot) => StringLenLattice::Bot
+//     }
+// }
+
+// fn str_replace(v1: StrVal, v2: StrVal, v3: StrVal) -> StrVal {
+//     match (v1, v2, v3) {
+//         (StrVal::Str(s1), StrVal::Str(s2), StrVal::Str(s3)) => StrVal::Str(s1.replace(&s2, &s3)),
+//         _ => StrVal::error()
+//     }
+// }
+
+// fn abs_str_replace(v1: StringLenLattice, v2: StringLenLattice, v3: StringLenLattice) -> StringLenLattice {
+//     // match (v1, v2, v3) {
+//     //     (StrVal::Str(s1), StrVal::Str(s2), StrVal::Str(s3)) => StrVal::Str(s1.replace(&s2, &s3)),
+//     //     _ => StrVal::error()
+//     // }
+//     StringLenLattice::Top
+// }
+
+// fn str_substr(v1: StrVal, v2: StrVal, v3: StrVal) -> StrVal {
+//     match (v1, v2, v3) {
+//         (StrVal::Str(s1), StrVal::Int(start), StrVal::Int(end)) => StrVal::Str(s1.chars().skip(start as usize).take((end - start) as usize).collect()),
+//         _ => StrVal::error()
+//     }
+// }
+
+// fn int_add(v1: StrVal, v2: StrVal) -> StrVal {
+//     match (v1, v2) {
+//         (StrVal::Int(i), StrVal::Int(j)) => StrVal::Int(i + j),
+//         _ => StrVal::error()
+//     }
+// }
+
+// fn int_sub(v1: StrVal, v2: StrVal) -> StrVal {
+//     match (v1, v2) {
+//         (StrVal::Int(i), StrVal::Int(j)) => StrVal::Int(i - j),
+//         _ => StrVal::error()
+//     }
+// }
+
+// fn str_len(v: StrVal) -> StrVal {
+//     match v {
+//         StrVal::Str(s) => StrVal::Int(s.chars().count() as i64),
+//         _ => StrVal::error()
+//     }
+// }
--- tests/interp_test.rs
+++ tests/interp_test.rs
@@ -4,117 +4,147 @@ use std::collections::HashMap;
 #[test]
 fn interp_int() {
     let env = HashMap::new();
-    let prog = Expr::Const(Value::Int(5));
+    let prog = Expr::Const(StrVal::Int(5));
 
-    assert_eq!(Value::Int(5), interp(&prog, &env));
+    assert_eq!(StrVal::Int(5), StrOpInterpreter::eval(&prog, &env));
 }
 
 #[test]
 fn interp_env() {
     let mut env = HashMap::new();
-    env.insert("x".to_string(), Value::Str("foo".to_string()));
+    env.insert("x".to_string(), StrVal::Str("foo".to_string()));
     let prog = Expr::Var("x".to_string());
 
-    assert_eq!(Value::Str("foo".to_string()), interp(&prog, &env));
+    assert_eq!(StrVal::Str("foo".to_string()), StrOpInterpreter::eval(&prog, &env));
 }
 
 #[test]
 fn interp_append() {
     let env = HashMap::new();
     let prog = Expr::Call(Box::new(
-        Func::Append(Expr::Const(Value::Str("hello ".to_string())),
-                     Expr::Const(Value::Str("world".to_string())))));
+        Func::Append(Expr::Const(StrVal::Str("hello ".to_string())),
+                     Expr::Const(StrVal::Str("world".to_string())))));
 
-    assert_eq!(Value::Str("hello world".to_string()), interp(&prog, &env));
+    assert_eq!(StrVal::Str("hello world".to_string()), StrOpInterpreter::eval(&prog, &env));
 }
 
 #[test]
-fn interp_err() {
+fn abs_interp_append_conc() {
     let env = HashMap::new();
     let prog = Expr::Call(Box::new(
-        Func::Append(Expr::Const(Value::Str("hello ".to_string())),
-                     Expr::Const(Value::Int(6)))));
+        Func::Append(Expr::Const(AbsValue::Conc(StrVal::Str("hello ".to_string()))),
+                     Expr::Const(AbsValue::Conc(StrVal::Str("world".to_string()))))));
 
-    assert_eq!(Value::Error, interp(&prog, &env));
+    assert_eq!(AbsValue::Conc(StrVal::Str("hello world".to_string())), StrLenInterp::eval(&prog, &env));
 }
 
 #[test]
-fn interp_replace() {
+fn abs_interp_append_abs() {
     let env = HashMap::new();
     let prog = Expr::Call(Box::new(
-        Func::Replace(Expr::Const(Value::Str("boop".to_string())),
-                      Expr::Const(Value::Str("b".to_string())),
-                      Expr::Const(Value::Str("p".to_string())))));
+        Func::Append(Expr::Const(AbsValue::Conc(StrVal::Str("hello ".to_string()))),
+                     Expr::Const(AbsValue::Abs(StrLenLat::Len(5))))));
 
-    assert_eq!(Value::Str("poop".to_string()), interp(&prog, &env));
+    assert_eq!(AbsValue::Abs(StrLenLat::Len(11)), StrLenInterp::eval(&prog, &env));
 }
 
 #[test]
-fn interp_substr() {
+fn abs_interp_append_err() {
     let env = HashMap::new();
     let prog = Expr::Call(Box::new(
-        Func::Substr(Expr::Const(Value::Str("Balloon".to_string())),
-                      Expr::Const(Value::Int(1)),
-                      Expr::Const(Value::Int(4)))));
+        Func::Append(Expr::Const(AbsValue::Conc(StrVal::Int(6))),
+                     Expr::Const(AbsValue::Abs(StrLenLat::Len(5))))));
 
-    assert_eq!(Value::Str("all".to_string()), interp(&prog, &env));
+    assert_eq!(AbsValue::Abs(StrLenLat::Len(11)), StrLenInterp::eval(&prog, &env));
 }
 
-#[test]
-fn interp_add() {
-    let env = HashMap::new();
-    let prog = Expr::Call(Box::new(
-        Func::Add(Expr::Const(Value::Int(1)),
-                  Expr::Const(Value::Int(4)))));
+// #[test]
+// fn interp_err() {
+//     let env = HashMap::new();
+//     let prog = Expr::Call(Box::new(
+//         Func::Append(Expr::Const(Value::Str("hello ".to_string())),
+//                      Expr::Const(Value::Int(6)))));
 
-    assert_eq!(Value::Int(5), interp(&prog, &env));
-}
+//     assert_eq!(Value::Error, interp(&prog, &env));
+// }
 
-#[test]
-fn interp_sub() {
-    let env = HashMap::new();
-    let prog = Expr::Call(Box::new(
-        Func::Sub(Expr::Const(Value::Int(1)),
-                  Expr::Const(Value::Int(4)))));
+// #[test]
+// fn interp_replace() {
+//     let env = HashMap::new();
+//     let prog = Expr::Call(Box::new(
+//         Func::Replace(Expr::Const(Value::Str("boop".to_string())),
+//                       Expr::Const(Value::Str("b".to_string())),
+//                       Expr::Const(Value::Str("p".to_string())))));
 
-    assert_eq!(Value::Int(-3), interp(&prog, &env));
-}
+//     assert_eq!(Value::Str("poop".to_string()), interp(&prog, &env));
+// }
 
-#[test]
-fn interp_len() {
-    let env = HashMap::new();
-    let prog = Expr::Call(Box::new(
-        Func::Len(Expr::Const(Value::Str("foo bar".to_string())))));
+// #[test]
+// fn interp_substr() {
+//     let env = HashMap::new();
+//     let prog = Expr::Call(Box::new(
+//         Func::Substr(Expr::Const(Value::Str("Balloon".to_string())),
+//                       Expr::Const(Value::Int(1)),
+//                       Expr::Const(Value::Int(4)))));
 
-    assert_eq!(Value::Int(7), interp(&prog, &env));
-}
+//     assert_eq!(Value::Str("all".to_string()), interp(&prog, &env));
+// }
 
-#[test]
-fn sygus_bikes() {
-    let prog = Expr::Call(Box::new(
-        Func::Substr(Expr::Var("arg0".to_string()),
-                     Expr::Const(Value::Int(0)),
-                     Expr::Call(Box::new(
-                        Func::Sub(Expr::Call(Box::new(
-                            Func::Len(Expr::Var("arg0".to_string())))),
-                        Expr::Const(Value::Int(3))))))));
+// #[test]
+// fn interp_add() {
+//     let env = HashMap::new();
+//     let prog = Expr::Call(Box::new(
+//         Func::Add(Expr::Const(Value::Int(1)),
+//                   Expr::Const(Value::Int(4)))));
 
-    let mut env = HashMap::new();
-    env.insert("arg0".to_string(), Value::Str("Ducati100".to_string()));
-    assert_eq!(Value::Str("Ducati".to_string()), interp(&prog, &env));
+//     assert_eq!(Value::Int(5), interp(&prog, &env));
+// }
 
-    env.insert("arg0".to_string(), Value::Str("Honda125".to_string()));
-    assert_eq!(Value::Str("Honda".to_string()), interp(&prog, &env));
+// #[test]
+// fn interp_sub() {
+//     let env = HashMap::new();
+//     let prog = Expr::Call(Box::new(
+//         Func::Sub(Expr::Const(Value::Int(1)),
+//                   Expr::Const(Value::Int(4)))));
 
-    env.insert("arg0".to_string(), Value::Str("Ducati250".to_string()));
-    assert_eq!(Value::Str("Ducati".to_string()), interp(&prog, &env));
+//     assert_eq!(Value::Int(-3), interp(&prog, &env));
+// }
 
-    env.insert("arg0".to_string(), Value::Str("Honda250".to_string()));
-    assert_eq!(Value::Str("Honda".to_string()), interp(&prog, &env));
+// #[test]
+// fn interp_len() {
+//     let env = HashMap::new();
+//     let prog = Expr::Call(Box::new(
+//         Func::Len(Expr::Const(Value::Str("foo bar".to_string())))));
 
-    env.insert("arg0".to_string(), Value::Str("Honda550".to_string()));
-    assert_eq!(Value::Str("Honda".to_string()), interp(&prog, &env));
+//     assert_eq!(Value::Int(7), interp(&prog, &env));
+// }
 
-    env.insert("arg0".to_string(), Value::Str("Ducati125".to_string()));
-    assert_eq!(Value::Str("Ducati".to_string()), interp(&prog, &env));
-}
+// #[test]
+// fn sygus_bikes() {
+//     let prog = Expr::Call(Box::new(
+//         Func::Substr(Expr::Var("arg0".to_string()),
+//                      Expr::Const(Value::Int(0)),
+//                      Expr::Call(Box::new(
+//                         Func::Sub(Expr::Call(Box::new(
+//                             Func::Len(Expr::Var("arg0".to_string())))),
+//                         Expr::Const(Value::Int(3))))))));
+
+//     let mut env = HashMap::new();
+//     env.insert("arg0".to_string(), Value::Str("Ducati100".to_string()));
+//     assert_eq!(Value::Str("Ducati".to_string()), interp(&prog, &env));
+
+//     env.insert("arg0".to_string(), Value::Str("Honda125".to_string()));
+//     assert_eq!(Value::Str("Honda".to_string()), interp(&prog, &env));
+
+//     env.insert("arg0".to_string(), Value::Str("Ducati250".to_string()));
+//     assert_eq!(Value::Str("Ducati".to_string()), interp(&prog, &env));
+
+//     env.insert("arg0".to_string(), Value::Str("Honda250".to_string()));
+//     assert_eq!(Value::Str("Honda".to_string()), interp(&prog, &env));
+
+//     env.insert("arg0".to_string(), Value::Str("Honda550".to_string()));
+//     assert_eq!(Value::Str("Honda".to_string()), interp(&prog, &env));
+
+//     env.insert("arg0".to_string(), Value::Str("Ducati125".to_string()));
+//     assert_eq!(Value::Str("Ducati".to_string()), interp(&prog, &env));
+// }
